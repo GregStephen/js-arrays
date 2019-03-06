d75cd288cd9e47534732925131adaa1a83d9a48c
Greg Stephen <GregAStephen@gmail.com>
2019-03-05 19:29:32 -0600

    did dino things

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-console.log ("sup lil bitch");
+const dinosaurs = [
+    {
+        dinoType: 'T-Rex',
+    },
+    {
+        dinoType: 'Triceratops',
+    },
+    {
+        dinoType: 'Stegosaurus',
+    }
+];
+
+
+// loop over the dinos
+// build up a domstring
+// print the domstring to the dom
+
+const printToDom = (divID, textToPrint) => {
+    const selectedDiv = document.getElementById(divID);
+    selectedDiv.innerHTML += textToPrint;
+};
+
+const buildDinosaurs = () => {
+    let domString = '';
+    for(let i = 0; i < dinosaurs.length; i++){
+        domString += `<h3>${dinosaurs[i].dinoType}</h3>`;
+        console.log("dinotype", dinosaurs[i].dinoType);
+    }
+    printToDom('dino-barn', domString)
+};
+
+const init = () => {
+    buildDinosaurs();
+};
+
+init();
